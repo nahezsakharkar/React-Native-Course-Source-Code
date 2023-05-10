701360629150be732df0671251577e05c47e9b31
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-10 13:32:24 +0530

    buttons

--- Part 1/Demo/DoneWithIt/App.js
+++ Part 1/Demo/DoneWithIt/App.js
@@ -9,17 +9,23 @@ import {
   TouchableOpacity,
   TouchableHighlight,
   TouchableNativeFeedback,
+  Button,
 } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello World!</Text>
-      <TouchableNativeFeedback onPress={() => console.log("Image Touched!")}>
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() => console.log("Button Pressed !")}
+      />
+      {/* <TouchableNativeFeedback onPress={() => console.log("Image Touched!")}>
         <View
           style={{ width: 300, height: 700, backgroundColor: "blue" }}
         ></View>
-      </TouchableNativeFeedback>
+      </TouchableNativeFeedback> */}
       {/* <Image
           source={{
             width: 200,
