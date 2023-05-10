0820b0cb1918e34c1eac4f71f88522caf0e2ef5a
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-10 13:22:24 +0530

    touchables

--- Part 1/Demo/DoneWithIt/App.js
+++ Part 1/Demo/DoneWithIt/App.js
@@ -7,21 +7,26 @@ import {
   Image,
   TouchableWithoutFeedback,
   TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
 } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello World!</Text>
-      <TouchableOpacity onPress={() => console.log("Image Touched!")}>
-        <Image
+      <TouchableNativeFeedback onPress={() => console.log("Image Touched!")}>
+        <View
+          style={{ width: 300, height: 700, backgroundColor: "blue" }}
+        ></View>
+      </TouchableNativeFeedback>
+      {/* <Image
           source={{
             width: 200,
             height: 300,
             uri: "https://picsum.photos/200/300",
           }}
-        />
-      </TouchableOpacity>
+        /> */}
       <StatusBar style="auto" />
     </SafeAreaView>
   );
