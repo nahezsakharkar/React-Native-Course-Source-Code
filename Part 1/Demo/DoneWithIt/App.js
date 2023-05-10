83e14bd4ade283bf4e40ea8d61b0a579263573fa
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-10 13:10:24 +0530

    image and some part of touchables

--- Part 1/Demo/DoneWithIt/App.js
+++ Part 1/Demo/DoneWithIt/App.js
@@ -1,24 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
-
-  // let x = 1;
-  // x.toString();
-
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello World!</Text>
+      <TouchableOpacity onPress={() => console.log("Image Touched!")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
