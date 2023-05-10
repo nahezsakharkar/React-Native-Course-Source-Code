6db10c5fa653f79c48d41c03b605203433c8f656
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-10 19:10:48 +0530

    some flexxbox

--- Part 1/Demo/DoneWithIt/App.js
+++ Part 1/Demo/DoneWithIt/App.js
@@ -1,26 +1,14 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import {
   StyleSheet,
-  Text,
   View,
   SafeAreaView,
-  Image,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  Button,
-  Alert,
   Platform,
   StatusBar,
-  Dimensions,
   useWindowDimensions,
 } from "react-native";
 
 export default function App() {
-  // console.log("Screen : ", Dimensions.get("screen"));
-  // console.log("Window : ", Dimensions.get("window"));
-
   const orientation = () => {
     const { height, width, scale, fontScale } = useWindowDimensions();
     return {
@@ -29,59 +17,30 @@ export default function App() {
     };
   };
 
-  const { landscape } = orientation();
+  // const { landscape } = orientation();
 
   return (
     <SafeAreaView style={[styles.container, containerStyle]}>
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
+          flex: 1,
+          flexDirection: "",
         }}
-      ></View>
+      >
+        <View
+          style={{ backgroundColor: "blue", width: 100, height: 100 }}
+        ></View>
+        <View
+          style={{ backgroundColor: "gold", width: 100, height: 100 }}
+        ></View>
+        <View
+          style={{ backgroundColor: "tomato", width: 100, height: 100 }}
+        ></View>
+      </View>
       <ExpoStatusBar style="auto" />
     </SafeAreaView>
   );
-}
-
-{
-  /* <Text>Hello World!</Text> */
-}
-{
-  /* <Button
-  title="Click Me"
-  onPress={() =>
-    Alert.prompt("Title", "Message", (text) => console.log(text))
-  }
-/> */
-}
-{
-  /* <Button
-  title="Click Me"
-  onPress={() =>
-    Alert.alert("Title", "Message", [
-      { text: "Yes", onPress: () => console.log("Yes Clicked.") },
-      { text: "No", onPress: () => console.log("No Clicked.") },
-    ])
-  }
-/> */
-}
-{
-  /* <TouchableNativeFeedback onPress={() => console.log("Image Touched!")}>
-  <View
-    style={{ width: 300, height: 700, backgroundColor: "blue" }}
-  ></View>
-</TouchableNativeFeedback> */
-}
-{
-  /* <Image
-    source={{
-      width: 200,
-      height: 300,
-      uri: "https://picsum.photos/200/300",
-    }}
-  /> */
 }
 
 const containerStyle = { backgroundColor: "orange" };
