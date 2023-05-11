5a475c3be127b3f778ac2427198cfb98df2498d3
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-11 15:49:24 +0530

    improving the welcome screen

--- Part 1/Demo/DoneWithIt/App.js
+++ Part 1/Demo/DoneWithIt/App.js
@@ -1,5 +1,3 @@
-import { View } from "react-native";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
--- Part 1/Demo/DoneWithIt/app/screens/WelcomeScreen.js
+++ Part 1/Demo/DoneWithIt/app/screens/WelcomeScreen.js
@@ -4,16 +4,22 @@ import AppButton from "../components/AppButton";
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={10}
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text>Sell What You Don't Need</Text>
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
-
-      <AppButton title="Login" onPress={() => console.log("Tapped !")} />
-      <View style={styles.registerButton}></View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title="Login" onPress={() => console.log("Tapped !")} />
+        <AppButton
+          title="Register"
+          color="secondary"
+          onPress={() => console.log("Tapped !")}
+        />
+      </View>
     </ImageBackground>
   );
 }
@@ -33,10 +39,14 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
   },
-  registerButton: {
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
+  },
+  buttonsContainer: {
+    padding: 20,
     width: "100%",
-    height: 70,
-    backgroundColor: "#4ecdc4",
   },
 });
 
