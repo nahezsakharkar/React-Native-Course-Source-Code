a7f491749e7b132830339ed822bc50b2b7923766
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-11 13:23:02 +0530

    platform specific code

--- Part 1/Demo/DoneWithIt/App.js
+++ Part 1/Demo/DoneWithIt/App.js
@@ -1,5 +1,5 @@
 import { View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppText from "./app/components/AppText";
 
 export default function App() {
   return (
@@ -10,7 +10,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <MaterialCommunityIcons name="email" size={60} color="dodgerblue" />
+      <AppText>I Love React Native!</AppText>
     </View>
   );
 }
