d35481704377ad9a5c992e64933f4711cc149197
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-11 17:13:35 +0530

    details listing screen

--- Part 1/Demo/DoneWithIt/App.js
+++ Part 1/Demo/DoneWithIt/App.js
@@ -1,22 +1,7 @@
-import { View } from "react-native";
-import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red Jacket for sale"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
 
 // const orientation = () => {
--- Part 1/Demo/DoneWithIt/app/screens/ListingDetailsScreen.js
+++ Part 1/Demo/DoneWithIt/app/screens/ListingDetailsScreen.js
@@ -1,11 +1,38 @@
+import AppText from "../components/AppText";
+import colors from "../config/colors";
 
+const { View, Image, StyleSheet } = require("react-native");
 
 function ListingDetailsScreen(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <View>
+      <Image style={styles.image} source={require("../assets/jacket.jpg")} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red Jacket For Sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+});
 
 export default ListingDetailsScreen;
