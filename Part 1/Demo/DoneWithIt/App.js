02954ed59ec223505940c7ebc2367a70c4e4eef9
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-12 20:28:22 +0530

    text input

--- Part 1/Demo/DoneWithIt/App.js
+++ Part 1/Demo/DoneWithIt/App.js
@@ -1,7 +1,25 @@
-import ListingsScreen from "./app/screens/ListingsScreen";
+import { useState } from "react";
+import { TextInput, Text } from "react-native";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Screen>
+      <Text>{firstName}</Text>
+      <TextInput
+        secureTextEntry
+        keyboardType="numeric"
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="First Name"
+        style={{
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      />
+    </Screen>
+  );
 }
 
 // const orientation = () => {
