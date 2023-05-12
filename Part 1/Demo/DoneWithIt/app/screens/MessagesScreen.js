2f5857ae3415cef150febc48d6042581eb9b2241
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-12 17:13:13 +0530

    refresh

--- Part 1/Demo/DoneWithIt/app/screens/MessagesScreen.js
+++ Part 1/Demo/DoneWithIt/app/screens/MessagesScreen.js
@@ -23,6 +23,7 @@ const initialMessages = [
 
 function MessagesScreen(props) {
   const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
 
   const handleDelete = (message) => {
     setMessages(messages.filter((m) => m.id != message.id));
@@ -45,6 +46,17 @@ function MessagesScreen(props) {
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              title: "T2",
+              description: "D2",
+              image: require("../assets/profile.jpg"),
+            },
+          ]);
+        }}
       />
     </Screen>
   );
