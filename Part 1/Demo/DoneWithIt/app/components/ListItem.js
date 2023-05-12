8b37419752a25d2e81bb9beb18dc028736de8214
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-12 19:06:08 +0530

    account screen

--- Part 1/Demo/DoneWithIt/app/components/ListItem.js
+++ Part 1/Demo/DoneWithIt/app/components/ListItem.js
@@ -10,7 +10,7 @@ function ListItem({
   title,
   subTitle,
   image,
-  ImageComponent,
+  IconComponent,
   onPress,
   renderRightActions,
 }) {
@@ -19,7 +19,7 @@ function ListItem({
       <Swipeable renderRightActions={renderRightActions}>
         <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
           <View style={styles.container}>
-            {ImageComponent}
+            {IconComponent}
             {image && <Image style={styles.image} source={image} />}
             <View style={styles.detailsContainer}>
               <AppText style={styles.title}>{title}</AppText>
@@ -38,6 +38,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     padding: 15,
+    backgroundColor: colors.white,
   },
   image: {
     width: 70,
--- Part 1/Demo/DoneWithIt/app/screens/AccountScreen.js
+++ Part 1/Demo/DoneWithIt/app/screens/AccountScreen.js
@@ -1,16 +1,70 @@
+import { View, StyleSheet, FlatList } from "react-native";
 import ListItem from "../components/ListItem";
 import Screen from "../components/Screen";
+import colors from "../config/colors";
+import Icon from "../components/Icon";
+import ListItemSeparator from "../components/ListItemSeparator";
+
+const menuItems = [
+  {
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
+    },
+  },
+  {
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+  },
+];
 
 function AccountScreen(props) {
   return (
-    <Screen>
+    <Screen style={styles.screen}>
+      <View style={styles.container}>
+        <ListItem
+          title="John Doe"
+          subTitle="johndoe@email.com"
+          image={require("../assets/profile.jpg")}
+        ></ListItem>
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItem) => menuItem.title}
+          ItemSeparatorComponent={ListItemSeparator}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+            />
+          )}
+        />
+      </View>
       <ListItem
-        title="John Doe"
-        subTitle="johndoe@email.com"
-        image={require("../assets/profile.jpg")}
-      ></ListItem>
+        title="Logout"
+        IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
+      />
     </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: colors.light,
+  },
+  container: {
+    marginVertical: 20,
+  },
+});
 
 export default AccountScreen;
