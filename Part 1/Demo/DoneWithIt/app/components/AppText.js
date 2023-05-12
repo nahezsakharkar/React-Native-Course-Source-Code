0a282700b2ccc357c860c12604c9df69f2c95cb5
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-12 21:04:42 +0530

    extracting default styles

--- Part 1/Demo/DoneWithIt/app/components/AppText.js
+++ Part 1/Demo/DoneWithIt/app/components/AppText.js
@@ -1,22 +1,9 @@
-import { Text, StyleSheet, Platform } from "react-native";
+import { Text } from "react-native";
+
+import defaultStyles from "../config/styles";
 
 function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
-
-const styles = StyleSheet.create({
-  text: {
-    ...Platform.select({
-      ios: {
-        fontSize: 20,
-        fontFamily: "Avenir",
-      },
-      android: {
-        fontSize: 18,
-        fontFamily: "Roboto",
-      },
-    }),
-  },
-});
 
 export default AppText;
