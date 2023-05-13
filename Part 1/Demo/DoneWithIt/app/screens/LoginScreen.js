2fa3d9e16867da5d64b301c493042c154f73ce31
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-13 14:03:22 +0530

    Building the Field Component

--- Part 1/Demo/DoneWithIt/app/screens/LoginScreen.js
+++ Part 1/Demo/DoneWithIt/app/screens/LoginScreen.js
@@ -2,10 +2,10 @@ import { StyleSheet, Image } from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
 
-import AppTextInput from "../components/AppTextInput";
 import AppButton from "../components/AppButton";
 import Screen from "../components/Screen";
 import ErrorMessage from "../components/ErrorMessage";
+import AppFormField from "../components/AppFormField";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string()
@@ -30,28 +30,24 @@ function LoginScreen(props) {
       >
         {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
           <>
-            <AppTextInput
+            <AppFormField
               autoCapitalize="none"
               autoCorrect={false}
               keyboardType="email-address"
               icon="email"
               placeholder="Email"
               textContentType="emailAddress"
-              onBlur={() => setFieldTouched("email")}
-              onChangeText={handleChange("email")}
+              name="email"
             />
-            <ErrorMessage error={errors.email} visible={touched.email} />
-            <AppTextInput
+            <AppFormField
               autoCapitalize="none"
               autoCorrect={false}
               secureTextEntry
               icon="lock"
               placeholder="Password"
               textContentType="password"
-              onBlur={() => setFieldTouched("password")}
-              onChangeText={handleChange("password")}
+              name="password"
             />
-            <ErrorMessage error={errors.password} visible={touched.password} />
             <AppButton title="Login" onPress={handleSubmit} />
           </>
         )}
