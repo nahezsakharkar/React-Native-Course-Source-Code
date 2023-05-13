9cce3d26eb377d10532fd2f43622dee8117e393e
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-13 16:37:43 +0530

    fixing the placeholder color

--- Part 1/Demo/DoneWithIt/app/components/AppTextInput.js
+++ Part 1/Demo/DoneWithIt/app/components/AppTextInput.js
@@ -13,7 +13,11 @@ function AppTextInput({ icon, ...otherProps }) {
           style={styles.icon}
         />
       )}
-      <TextInput style={defaultStyles.text} {...otherProps} />
+      <TextInput
+        placeholderTextColor={defaultStyles.colors.medium}
+        style={defaultStyles.text}
+        {...otherProps}
+      />
     </View>
   );
 }
