1e7d1bb188fc114327b06b5d7d37b6320a75c5cd
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-24 02:25:00 +0530

    Extending the Api Layer

--- Part 1/Demo/DoneWithIt/app/api/listings.js
+++ Part 1/Demo/DoneWithIt/app/api/listings.js
@@ -3,6 +3,29 @@ import client from "./client";
 const endpoint = "/listings";
 const getListings = () => client.get(endpoint);
 
+const addListing = (listing) => {
+  const data = new FormData();
+  data.append("title", listing.title);
+  data.append("description", listing.description);
+  data.append("price", listing.price);
+  data.append("category", listing.category);
+
+  listing.images.forEach((image, index) => {
+    data.append("images", {
+      name: "image" + index,
+      type: "image/jpeg",
+      uri: image,
+    });
+  });
+
+  if (listing.location) {
+    data.append("location", JSON.stringify(listing.location));
+  }
+
+  return client.post(endpoint, data);
+};
+
 export default {
   getListings,
+  addListing,
 };
