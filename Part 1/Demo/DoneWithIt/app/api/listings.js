21a19e36c40c7207dbb9180c821d9979151ceb31
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-05-25 00:21:59 +0530

    Tracking Upload Progress

--- Part 1/Demo/DoneWithIt/app/api/listings.js
+++ Part 1/Demo/DoneWithIt/app/api/listings.js
@@ -3,26 +3,28 @@ import client from "./client";
 const endpoint = "/listings";
 const getListings = () => client.get(endpoint);
 
-const addListing = (listing) => {
+const addListing = (listing, onUploadProgress) => {
   const data = new FormData();
   data.append("title", listing.title);
-  data.append("description", listing.description);
   data.append("price", listing.price);
-  data.append("category", listing.category);
+  data.append("categoryId", listing.category.value);
+  data.append("description", listing.description);
 
-  listing.images.forEach((image, index) => {
+  listing.images.forEach((image, index) =>
     data.append("images", {
       name: "image" + index,
       type: "image/jpeg",
       uri: image,
-    });
-  });
+    })
+  );
 
-  if (listing.location) {
+  if (listing.location)
     data.append("location", JSON.stringify(listing.location));
-  }
 
-  return client.post(endpoint, data);
+  return client.post(endpoint, data, {
+    onUploadProgress: (progress) =>
+      onUploadProgress(progress.loaded / progress.total),
+  });
 };
 
 export default {
--- Part 1/Demo/DoneWithIt/app/screens/ListingEditScreen.js
+++ Part 1/Demo/DoneWithIt/app/screens/ListingEditScreen.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
-
+import listingsApi from "../api/listings";
 import {
   AppForm,
   AppFormField,
@@ -21,14 +21,75 @@ const validationSchema = Yup.object().shape({
 });
 
 const categories = [
-  { label: "Furniture", value: 1, backgroundColor: "red", icon: "apps" },
-  { label: "Clothing", value: 2, backgroundColor: "green", icon: "email" },
-  { label: "Camera", value: 3, backgroundColor: "blue", icon: "lock" },
+  {
+    backgroundColor: "#fc5c65",
+    icon: "floor-lamp",
+    label: "Furniture",
+    value: 1,
+  },
+  {
+    backgroundColor: "#fd9644",
+    icon: "car",
+    label: "Cars",
+    value: 2,
+  },
+  {
+    backgroundColor: "#fed330",
+    icon: "camera",
+    label: "Cameras",
+    value: 3,
+  },
+  {
+    backgroundColor: "#26de81",
+    icon: "cards",
+    label: "Games",
+    value: 4,
+  },
+  {
+    backgroundColor: "#2bcbba",
+    icon: "shoe-heel",
+    label: "Clothing",
+    value: 5,
+  },
+  {
+    backgroundColor: "#45aaf2",
+    icon: "basketball",
+    label: "Sports",
+    value: 6,
+  },
+  {
+    backgroundColor: "#4b7bec",
+    icon: "headphones",
+    label: "Movies & Music",
+    value: 7,
+  },
+  {
+    backgroundColor: "#a55eea",
+    icon: "book-open-variant",
+    label: "Books",
+    value: 8,
+  },
+  {
+    backgroundColor: "#778ca3",
+    icon: "application",
+    label: "Other",
+    value: 9,
+  },
 ];
 
 function ListingEditScreen() {
   const location = useLocation();
-  
+  const [progress, setProgress] = useState(0);
+
+  const handleSubmit = async (listing) => {
+    const result = await listingsApi.addListing(
+      { ...listing, location },
+      (progress) => setProgress(progress)
+    );
+    if (!result.ok) return alert("Could not save the listing");
+    alert("Success");
+  };
+
   return (
     <Screen style={styles.container}>
       <AppForm
@@ -39,7 +100,7 @@ function ListingEditScreen() {
           category: null,
           images: [],
         }}
-        onSubmit={(values) => console.log(location)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
         <AppFormImagePicker name="images" />
